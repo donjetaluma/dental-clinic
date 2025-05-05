e0088c741298d55589458e620ea39fa5fc6ce67f
donjetaluma <donjetaluma123@gmail.com>
2025-05-05 22:48:03 +0200

    admin-blade

--- resources/views/admin/patients-create.blade.php
+++ resources/views/admin/patients-create.blade.php
@@ -7,7 +7,7 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            @livewire('creates-patient')
+            @livewire('patients-create')
         </div>
     </div>
 </x-app-layout>
